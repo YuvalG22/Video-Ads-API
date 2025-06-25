1a72cf90ab3a7e5a382599a9bcfe9538e1895706
Yuval Ganon <yuvalpay22@gmail.com>
2025-06-25 12:50:09 +0300

    fixes

--- models/Ad.js
+++ models/Ad.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const adSchema = new mongoose.Schema({
   title: String,
   videoUrl: String,
-  thumbnailUrl: String,
   advertiser: String,
+  category: String,
   duration: Number,
   impressions: { type: Number, default: 0 },
   clicks: { type: Number, default: 0 },
